ca43f6888c974a2e69e0d1b0f7446bc72a7f9037
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-04 14:58:45 +0530

    fanout

--- src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/ConfigureRabbitMQ.java
+++ src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/ConfigureRabbitMQ.java
@@ -11,12 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class ConfigureRabbitMQ {
     public static final String TOPIC_QUEUE = "pradipta.bowlertopicqueue";
     public static final String DIRECT_QUEUE = "pradipta.bowlerdirectqueue";
+    public static final String FANOUT_QUEUE_1 = "pradipta.bowlerfanoutqueue1";
+    public static final String FANOUT_QUEUE_2 = "pradipta.bowlerfanoutqueue2";
+    public static final String FANOUT_QUEUE_3 = "pradipta.bowlerfanoutqueue3";
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-        simpleMessageListenerContainer.setQueueNames(TOPIC_QUEUE, DIRECT_QUEUE);
+        simpleMessageListenerContainer.setQueueNames(TOPIC_QUEUE, DIRECT_QUEUE, FANOUT_QUEUE_1, FANOUT_QUEUE_2, FANOUT_QUEUE_3);
         simpleMessageListenerContainer.setMessageListener(listenerAdapter);
         return simpleMessageListenerContainer;
     }
