9d64666be20618596b5238665f859ae880c513e7
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-06 12:12:22 +0530

    unused code

--- src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/ConsumerHandler.java
+++ src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/ConsumerHandler.java
@@ -1,36 +1,12 @@
 package com.pradipta.batsmanrabbitmqconsumer.rabbitmq.consumer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Queue;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLOutput;
 
 @Service
 @Slf4j
 public class ConsumerHandler {
     public void handleMessage(String messageBody){
-        log.info("Handling message:");
-        log.info(messageBody);
-    }
-
-    public void handleMessageForFanout1(String messageBody){
-        System.out.println("This is working");
-        log.info("Fanout 1");
-        log.info("Handling message:");
-        log.info(messageBody);
-    }
-
-    public void handleMessageForFanout2(String messageBody){
-        System.out.println("This is working");
-        log.info("Fanout 2");
-        log.info("Handling message:");
-        log.info(messageBody);
-    }
-
-    public void handleMessageForFanout3(String messageBody){
-        System.out.println("This is working");
-        log.info("Fanout 3");
         log.info("Handling message:");
         log.info(messageBody);
     }
--- src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/FanoutHandler.java
+++ src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/FanoutHandler.java
@@ -2,31 +2,25 @@ package com.pradipta.batsmanrabbitmqconsumer.rabbitmq.consumer;
 
 import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Queue;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLOutput;
 
 @Service
 @Slf4j
 public class FanoutHandler {
     @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout1(String messageBody){
-        System.out.println("This is working");
         log.info("Fanout 1");
         log.info("Handling message:");
         log.info(messageBody);
     }
     @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout2(String messageBody){
-        System.out.println("This is working");
         log.info("Fanout 2");
         log.info("Handling message:");
         log.info(messageBody);
     }
     @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout3(String messageBody){
-        System.out.println("This is working");
         log.info("Fanout 3");
         log.info("Handling message:");
         log.info(messageBody);
