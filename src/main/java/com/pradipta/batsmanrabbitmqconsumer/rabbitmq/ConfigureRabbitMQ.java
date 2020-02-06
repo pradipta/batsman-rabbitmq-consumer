342da33b7c84ebd1828a4eaed014e7b6f57f6de2
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-02-06 11:59:44 +0530

    separated queues

--- src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/ConfigureRabbitMQ.java
+++ src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/ConfigureRabbitMQ.java
@@ -1,9 +1,11 @@
 package com.pradipta.batsmanrabbitmqconsumer.rabbitmq;
 
 import com.pradipta.batsmanrabbitmqconsumer.rabbitmq.consumer.ConsumerHandler;
+import com.pradipta.batsmanrabbitmqconsumer.rabbitmq.consumer.FanoutHandler;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +18,7 @@ public class ConfigureRabbitMQ {
     public static final String FANOUT_QUEUE_3 = "pradipta.bowlerfanoutqueue3";
 
     @Bean
-    SimpleMessageListenerContainer containerTopic(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
+    SimpleMessageListenerContainer containerTopic(ConnectionFactory connectionFactory, @Qualifier("Normal") MessageListenerAdapter listenerAdapter){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
         simpleMessageListenerContainer.setQueueNames(TOPIC_QUEUE);
@@ -25,7 +27,7 @@ public class ConfigureRabbitMQ {
     }
 
     @Bean
-    SimpleMessageListenerContainer containerDirect(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
+    SimpleMessageListenerContainer containerDirect(ConnectionFactory connectionFactory, @Qualifier("Normal") MessageListenerAdapter listenerAdapter){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
         simpleMessageListenerContainer.setQueueNames(DIRECT_QUEUE);
@@ -34,7 +36,7 @@ public class ConfigureRabbitMQ {
     }
 
     @Bean
-    SimpleMessageListenerContainer containerFan1(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter1){
+    SimpleMessageListenerContainer containerFan1(ConnectionFactory connectionFactory, @Qualifier("Fanout1") MessageListenerAdapter listenerAdapter1){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
         simpleMessageListenerContainer.setQueueNames(FANOUT_QUEUE_1);
@@ -43,7 +45,7 @@ public class ConfigureRabbitMQ {
     }
 
     @Bean
-    SimpleMessageListenerContainer containerFan2(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter2){
+    SimpleMessageListenerContainer containerFan2(ConnectionFactory connectionFactory, @Qualifier("Fanout2") MessageListenerAdapter listenerAdapter2){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
         simpleMessageListenerContainer.setQueueNames(FANOUT_QUEUE_2);
@@ -52,15 +54,34 @@ public class ConfigureRabbitMQ {
     }
 
     @Bean
-    SimpleMessageListenerContainer containerFan3(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter3){
+    SimpleMessageListenerContainer containerFan3(ConnectionFactory connectionFactory, @Qualifier("Fanout3") MessageListenerAdapter listenerAdapter){
         SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
         simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
         simpleMessageListenerContainer.setQueueNames(FANOUT_QUEUE_3);
-        simpleMessageListenerContainer.setMessageListener(listenerAdapter3);
+        simpleMessageListenerContainer.setMessageListener(listenerAdapter);
         return simpleMessageListenerContainer;
     }
 
     @Bean
+    @Qualifier("Fanout1")
+    MessageListenerAdapter listenerAdapterForFanout1(FanoutHandler handler){
+        return new MessageListenerAdapter(handler, "handleMessageForFanout1");
+    }
+
+    @Bean
+    @Qualifier("Fanout2")
+    MessageListenerAdapter listenerAdapterForFanout2(FanoutHandler handler){
+        return new MessageListenerAdapter(handler, "handleMessageForFanout2");
+    }
+
+    @Bean
+    @Qualifier("Fanout3")
+    MessageListenerAdapter listenerAdapterForFanout3(FanoutHandler handler){
+        return new MessageListenerAdapter(handler, "handleMessageForFanout3");
+    }
+
+    @Bean
+    @Qualifier("Normal")
     MessageListenerAdapter listenerAdapter(ConsumerHandler handler){
         return new MessageListenerAdapter(handler, "handleMessage");
     }
