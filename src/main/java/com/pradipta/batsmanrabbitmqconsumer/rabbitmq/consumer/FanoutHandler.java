82947a1f330a96f45350e9aa27ae55757771e180
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-02-06 12:02:36 +0530

    Listener seperation (#4)
    * seperate containers
    
    * separated queues

--- src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/FanoutHandler.java
+++ src/main/java/com/pradipta/batsmanrabbitmqconsumer/rabbitmq/consumer/FanoutHandler.java
@@ -1,5 +1,6 @@
 package com.pradipta.batsmanrabbitmqconsumer.rabbitmq.consumer;
 
+import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
 import org.springframework.stereotype.Service;
@@ -8,26 +9,22 @@ import java.sql.SQLOutput;
 
 @Service
 @Slf4j
-public class ConsumerHandler {
-    public void handleMessage(String messageBody){
-        log.info("Handling message:");
-        log.info(messageBody);
-    }
-
+public class FanoutHandler {
+    @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout1(String messageBody){
         System.out.println("This is working");
         log.info("Fanout 1");
         log.info("Handling message:");
         log.info(messageBody);
     }
-
+    @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout2(String messageBody){
         System.out.println("This is working");
         log.info("Fanout 2");
         log.info("Handling message:");
         log.info(messageBody);
     }
-
+    @Synchronized           //This is just so that all three lines for one queue is printed together
     public void handleMessageForFanout3(String messageBody){
         System.out.println("This is working");
         log.info("Fanout 3");
